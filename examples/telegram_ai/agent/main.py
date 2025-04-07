df7efd819f85e96ed9c25166c71d496e112158ee
johnny-emp <johnny@empyrealsdk.com>
2025-04-06 21:48:43 -0700

    telegram utils

--- examples/telegram_ai/agent/main.py
+++ examples/telegram_ai/agent/main.py
@@ -1,14 +1,12 @@
-import os
-
 from dotenv import load_dotenv
 from emp_agents import AgentBase
 from emp_agents.providers import OpenAIModelType, OpenAIProvider
 from telegram import Update
-from telegram.constants import ChatType
 from telegram.ext import ContextTypes, filters
 
 from emp_hooks import manager
 from emp_hooks.handlers.telegram import on_message
+from emp_hooks.utils.telegram import is_group_chat
 
 from .prompts import GROUP_CHAT_PROMPT, PRIVATE_CHAT_PROMPT
 from .services import chat_service, message_service, user_service
@@ -23,37 +21,18 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not (update.message and update.message.from_user):
         return
 
-    is_group_chat = update.message.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]
-
-    if is_group_chat:
-        agent = AgentBase(
-            prompt=GROUP_CHAT_PROMPT,
-            sync_tools=True,
-            provider=OpenAIProvider(
-                api_key=os.environ["OPENAI_API_KEY"],
-                default_model=OpenAIModelType.gpt4o,
-            ),
-        )
-    else:
-        agent = AgentBase(
-            prompt=PRIVATE_CHAT_PROMPT,
-            sync_tools=True,
-            provider=OpenAIProvider(
-                api_key=os.environ["OPENAI_API_KEY"],
-                default_model=OpenAIModelType.gpt4o,
-            ),
-        )
+    prompt = GROUP_CHAT_PROMPT if is_group_chat(update) else PRIVATE_CHAT_PROMPT
+    agent = AgentBase(
+        prompt=prompt,
+        provider=OpenAIProvider(
+            default_model=OpenAIModelType.gpt4o,
+        ),
+    )
 
     # add user and message to database
     chat = chat_service.store_chat(update.message)
     db_user = user_service.store_user(update.message.from_user)
-    message_service.get_or_create(
-        chat_id=chat.id,
-        message_id=update.message.message_id,
-        text=update.message.text,
-        user_id=db_user.id,
-        timestamp=update.message.date,
-    )
+    message_service.add_message(chat, db_user, update.message)
 
     # create response using conversation history
     response = await agent.answer(
@@ -66,15 +45,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # if agent has something to say, send it
     if response != "NO RESPONSE":
         response_msg = await update.message.reply_text(response)
-
-        # add agent message to database
-        message_service.get_or_create(
-            chat_id=chat.id,
-            message_id=response_msg.message_id,
-            text=response,
-            user_id=bot_user.id,
-            timestamp=response_msg.date,
-        )
+        message_service.add_message(chat, bot_user, response_msg)
 
 
 @on_message(command="test")
